35a430a5582301d32fa3a0985767fe5d373cb2ff
Mateusz Percinski <mateusz.percinski@gmail.com>
2016-02-26 08:53:22 +0100

    sensors added, LEDs in interrupts

--- hermes/arduino/arduino_uno/arduino_uno.ino
+++ hermes/arduino/arduino_uno/arduino_uno.ino
@@ -3,19 +3,27 @@ Arduino Servo Test sketch
 */
 #include "MsTimer2.h"
 #include <Servo.h>
+
+#define sensorR A1
+#define sensorL A0
+
 Servo servoL1; // Define our Servo
 Servo servoR1;
 Servo servoL2; // Define our Servo
 Servo servoR2;
 
-int angle=30;
+int oneStep=90;
 int cpL1=90;
-int cpR1=90;
-int cpL2=90;
-int cpR2=90;
-int angleL=30;
-int angleR=30;
+int cpR1=100;
+int cpL2=90-oneStep/2;
+int cpR2=90-oneStep/2;
+int angleL=45;
+int angleR=45;
 int ledCounter=0;
+int mDelay=8;
+
+int d_oneStep=0;
+float k=0.05;
 
 void setup()
 {
@@ -24,8 +32,6 @@ void setup()
    servoL2.attach(9); // servo on digital pin 12
    servoR2.attach(8); // servo on digital pin 13
 
-   startPosition();
-
    pinMode(7, OUTPUT);
    digitalWrite(7, HIGH);
    pinMode(6, OUTPUT);
@@ -33,17 +39,23 @@ void setup()
    pinMode(4, OUTPUT);
    pinMode(3, OUTPUT);
    pinMode(2, OUTPUT);
-//   Timer0.initialize(1000000/6);         // initialize timer1, and set a 1/2 second period
-//   Timer0.attachInterrupt(callback1);  // attaches callback() as a timer overflow interrupt
-    MsTimer2::set(500, callback1); // 500ms period
-    MsTimer2::start();
-}
+   pinMode(1, INPUT);
+   Serial.begin(9600);
+   
+   MsTimer2::set(1000/6, callback); // 500ms period
+   startPosition();
 
+   while(digitalRead(1)==HIGH){ 
+   }
+   Serial.println("start");
+   delay(2000);
+   
+   MsTimer2::start();
+   firstStep(mDelay); 
+}
 void loop()
 {
-
-     go(10);
-   
+     go(mDelay);
 }
 
 void startPosition(){
@@ -54,13 +66,55 @@ void startPosition(){
   delay(2000);
 }
 
-void firstStep(){
-  
+void firstStep(int mDelay){
+  // przechyl na lewo
+    for(int i=0; i<angleL; i++){
+      servoR1.write(cpR1+i);
+      servoL1.write(cpL1+i);
+    delay(mDelay);
+    }
+    servoR2.write(cpR2);
+    servoL2.write(cpL2); 
+   
+    // obroc na stopie
+    for(int i=0; i<oneStep/2; i++){
+      servoL2.write(cpL2+i);
+      servoR2.write(cpR2+i);
+      delay(mDelay);
+    }
+    // wyprostuj sie
+    for(int i=angleL; i>0; i--){
+      servoR1.write(cpR1+i);
+      servoL1.write(cpL1+i);
+      delay(mDelay);
+    }
+    // przechyl na prawo
+    for(int i=0; i<angleL; i++){
+      servoR1.write(cpR1-i);
+      servoL1.write(cpL1-i);
+    delay(mDelay);
+    }
+    servoR2.write(cpR2);
+    servoL2.write(cpL2); 
+   
+    // obroc na stopie
+    for(int i=0; i<oneStep; i++){
+      servoL2.write(cpL2+oneStep/2-i);
+      servoR2.write(cpR2+oneStep/2-i);
+      delay(mDelay);
+    }
+    // wyprostuj sie
+    for(int i=angleL; i>0; i--){
+      servoR1.write(cpR1-i);
+      servoL1.write(cpL1-i);
+      delay(mDelay);
+    }
 }
 
 void go(int mDelay){
+  int d_maxL=analogRead(sensorL);
+  int d_maxR=analogRead(sensorR);
   
-
    // przechyl na lewo
     for(int i=0; i<angleL; i++){
       servoR1.write(cpR1+i);
@@ -71,9 +125,18 @@ void go(int mDelay){
     servoL2.write(cpL2); 
    
     // obroc na stopie
-    for(int i=0; i<90; i++){
-      servoL2.write(cpL2+i);
-      servoR2.write(cpR2+i);
+    Serial.println(oneStep+d_oneStep);
+    for(int i=0; i<oneStep+d_oneStep; i++){
+      servoL2.write(cpL2-oneStep/2+i);
+      servoR2.write(cpR2-oneStep/2+i);
+
+      if(analogRead(sensorR)>d_maxR){
+        d_maxR=analogRead(sensorR);
+      }
+      if(analogRead(sensorL)>d_maxL){
+        d_maxL=analogRead(sensorL);
+      }
+      
       delay(mDelay);
     }
     // wyprostuj sie
@@ -89,9 +152,18 @@ void go(int mDelay){
     delay(mDelay);
     } 
     // obroc na stopie
-    for(int i=90; i>0; i--){
-      servoL2.write(cpL2+i);
-      servoR2.write(cpR2+i);
+    Serial.println(oneStep-d_oneStep);
+    for(int i=0; i<oneStep-d_oneStep; i++){
+      servoL2.write(cpL2+oneStep/2-i);
+      servoR2.write(cpR2+oneStep/2-i);
+
+      if(analogRead(sensorR)>d_maxR){
+        d_maxR=analogRead(sensorR);
+      }
+      if(analogRead(sensorL)>d_maxL){
+        d_maxL=analogRead(sensorL);
+      }
+      
       delay(mDelay);
     }
      // wyprostuj sie
@@ -100,6 +172,8 @@ void go(int mDelay){
       servoL1.write(cpL1-i);
       delay(mDelay);
     }  
+
+    d_oneStep=k*(d_maxL-d_maxR);
 }
 
 void callback()
